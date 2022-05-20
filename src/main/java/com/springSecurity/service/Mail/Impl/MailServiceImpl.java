453b9e41da666d5e9f62d27ea43219b1f9e7f809
小森森 <1309463811@qq.com>
2022-05-20 15:54:15 +0800

    今天完善登录验证功能

--- src/main/java/com/springSecurity/service/Mail/Impl/MailServiceImpl.java
+++ src/main/java/com/springSecurity/service/Mail/Impl/MailServiceImpl.java
@@ -12,6 +12,9 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 @Service
 
 public class MailServiceImpl implements MailService {
@@ -24,23 +27,48 @@ public class MailServiceImpl implements MailService {
     @Autowired
     private MailUtil mailUtil;
 
+    private ExecutorService fixedThreadPool = Executors.newFixedThreadPool(5);
+
+
+
     @Override
     public String sendMsgToMq(String msg) {
         //消息生产
         rabbitTemplate.convertAndSend(RabbitConfiguration.EXCHANGE_A, RabbitConfiguration.ROUTINGKEY_B, msg);
         logger.info("rabbitmq消息已经发送到交换机, 等待交换机接收..." + msg);
+
+
+
+
+
         return "success";
     }
 
 
     @RabbitHandler
-    @RabbitListener(queues = RabbitConfiguration.QUEUE_B,containerFactory="rabbitListenerContainerFactory")
+    @RabbitListener(queues = RabbitConfiguration.QUEUE_B)
     public void sendEMail(String msg){
-        MailBean mailBean = new MailBean();
-        mailBean.setSubject("极限云上线测试");
-        mailBean.setText(msg);
-        //发送邮件
-        mailUtil.sendMail(mailBean);
-        logger.info("已发送邮件");
+        logger.info("接收到发送邮件任务:{}", msg);
+        fixedThreadPool.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    MailBean mailBean = new MailBean();
+                    mailBean.setSubject("测试");
+                    mailBean.setText(msg);
+                    //发送邮件
+                    mailUtil.sendMail(mailBean);
+                } catch (Exception e) {
+                    logger.error(e.getMessage(), e);
+                }
+            }
+        });
+
+
+
+
+
+
+
     }
 }
